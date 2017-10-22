f374e6bff99604ba8b19a1af0e8ad0a86ca9a835
Paraxdq <dennisb.1790@gmail.com>
2017-10-22 19:33:34 +0200

    Added project.java

---
+++ SE1_2.2/src/Project.java
@@ -0,0 +1,12 @@
+
+public class Project {
+
+	public Project(String[][] strings) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public boolean isWellSorted(String[] strings) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
